1a67c3087d10d4eafdd49263cd065bb81c1787b8
poporthailand <ismat_rr@hotmail.com>
2021-04-08 21:47:17 +0700

    save

--- Nodejs/routes/index.js
+++ Nodejs/routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+  res.sendFile( 'D:/IT2RC/term2/Final/JAVASCRIPT PROGRAMMING/Project_BubbleTea_Nodejs/Nodejs/views/index.html');
 });
 
 module.exports = router;
