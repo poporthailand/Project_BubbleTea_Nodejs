3dcab74370f29e4847c4c438277e9463cbd23615
poporthailand <ismat_rr@hotmail.com>
2021-04-08 22:38:27 +0700

    s

--- main.js
+++
@@ -1,9 +0,0 @@
-const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks',
-            image: './assets/images/socks_green.jpg',
-            url: 'https://www.vuemastery.com'
-        }
-    }
-})
